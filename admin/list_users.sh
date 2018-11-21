984602f8ad750f519adf2290d4ecee9514a4982f
Enzo Gladiadis <30397502+Tr0j4n9@users.noreply.github.com>
2018-11-21 13:52:25 +1300

    Update list_users.sh

--- admin/list_users.sh
+++ admin/list_users.sh
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-db=txbits
+db=flameexchange
 sql="sudo -u postgres psql"
 
 if [ -n "$1" ]; then

19f7960d84983650759ebdb5d35c2483f13c1c6b
Enzo Gladiadis <30397502+Tr0j4n9@users.noreply.github.com>
2018-12-04 22:17:03 +1300

    Update UserModel.scala

--- txbits/app/models/UserModel.scala
+++ txbits/app/models/UserModel.scala
@@ -159,15 +159,15 @@ class UserModel(val db: String = "default") {
       row[Option[Boolean]]("on_mailing_list"),
       row[Option[Boolean]]("tfa_enabled"),
       row[Option[String]]("pgp"),
-      row[String]("language1")) match {
+      row[String]("language")) match {
         case (Some(id: Long),
           Some(email: String),
           Some(verification: Int),
           Some(on_mailing_list: Boolean),
           Some(tfa_enabled: Boolean),
           pgp: Option[String],
-          language1: String) =>
-          Some(SocialUser(id, email, verification, language1, on_mailing_list, tfa_enabled, pgp))
+          language: String) =>
+          Some(SocialUser(id, email, verification, language, on_mailing_list, tfa_enabled, pgp))
         case _ =>
           None
       }

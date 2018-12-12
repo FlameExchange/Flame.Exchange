5a951d761ffb52ad60cef20c181bcca7ce0db670
Enzo Gladiadis <30397502+Tr0j4n9@users.noreply.github.com>
2018-12-13 08:42:05 +1300

    Adding PirateCash

--- txbits/app/controllers/IAPI/CryptoAddress.scala
+++ txbits/app/controllers/IAPI/CryptoAddress.scala
@@ -100,6 +100,16 @@ object CryptoAddress {
 
     val getPaymentProtocolId: String = PAYMENT_PROTOCOL_ID_MAINNET
   }
+  
+    private val Piratecash = new NetworkParameters {
+    id = ID_MAINNET
+    port = 11888
+    addressHeader = 55
+    tp2shHeader = 55
+    acceptableAddressCodes = Array[Int](addressHeader, p2shHeader)
+
+    val getPaymentProtocolId: String = PAYMENT_PROTOCOL_ID_MAINNET
+  }
 
   def isValid(address: String, currency: String, testnet: Boolean): Boolean = {
     val network = currency match {
@@ -111,6 +121,7 @@ object CryptoAddress {
       case "PPC" => Peercoin
       case "XPM" if testnet => PrimecoinTestnet
       case "XPM" => Primecoin
+      case "PIRATE" => Piratecash
       case _ =>
         return false
     }

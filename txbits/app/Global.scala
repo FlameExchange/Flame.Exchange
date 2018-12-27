69bede58b3361191751e53f0549e6db8a36b0bd6
Enzo Gladiadis <30397502+Tr0j4n9@users.noreply.github.com>
2018-12-27 15:57:16 +1300

    Update Global.scala

--- txbits/app/Global.scala
+++ txbits/app/Global.scala
@@ -82,7 +82,7 @@ package object globals {
     "litecoin" -> Wallet.CryptoCurrency.LTC,
     "piratecash" -> Wallet.CryptoCurrency.PIRATE,
     "rebooted" -> Wallet.CryptoCurrency.BOOT,
-    "eazycoin" -> Wallet.CryptoCurrency.EZY)
+    "eazynode" -> Wallet.CryptoCurrency.EZY)
 
   val enabledCurrencies = currencies.filter(c =>
     Play.current.configuration.getBoolean("wallet.%s.enabled".format(c._1)).getOrElse(false))
